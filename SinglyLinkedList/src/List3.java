b37f8342d92a5f0f660a5e1e538610c5830ab82c
hientran-sudo <hien.tran@student.fairfield.edu>
2021-09-01 15:52:46 -0400

    Count number of times item appears in the list

---
+++ SinglyLinkedList/src/List3.java
@@ -0,0 +1,56 @@
+import java.util.Scanner;
+
+public class List3 {
+	Node head;
+
+	public static void main(String[] args) {
+		List3 list = new List3();
+		list.head = new Node (1);
+		list.add(2);
+		list.add(3);
+		list.add(4);
+		list.add(2);
+		list.add(4);
+		list.add(5);
+		list.add(1);
+		
+		list.print();
+		Scanner scan = new Scanner(System.in);		
+		System.out.println("Enter a number  ");
+		int input = scan.nextInt();
+		System.out.println("Number appears " + list.count(input) + " times");
+		
+
+	}
+	public void add (int a) {
+		Node n = new Node (a);		
+		n.next = null;
+		Node tail = head;
+		while (tail.next != null) {
+			tail = tail.next;
+		}
+		tail.next = n;
+		return;
+	}
+	
+	public void print() {
+		Node n = head;
+		while (n!= null) {
+			System.out.println(n.data);
+			n=n.next;
+		}
+	}
+	
+	public int count(int i) {
+		int count = 0;
+		Node n = head;
+		while (n!=null) {
+			if (n.data==i) {
+				count++;
+			}
+			n=n.next;
+		}		
+		return count;		
+	}
+
+}
