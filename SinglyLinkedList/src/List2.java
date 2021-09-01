5a84e8a99e7c8c10ffaeac61fee2e420fe01fb9c
hientran-sudo <hien.tran@student.fairfield.edu>
2021-09-01 14:57:14 -0400

    find middle node

--- SinglyLinkedList/src/List2.java
+++ SinglyLinkedList/src/List2.java
@@ -5,14 +5,28 @@ public class List2 {
 		List2 list = new List2();
 		list.head = new Node (1);
 		Node second = new Node (2);
-		Node third = new Node (3);
+		//Node third = new Node (3);
+		//Node fourth = new Node (4);
+		//Node fifth = new Node (6);
 		
 		list.head.next = second;
-		second.next = third;
+		//second.next = third;
+		//third.next=fourth;
+		//fourth.next = fifth;
 		
 		list.print();
 		System.out.println("length of list " + list.findlength());
 		list.findnode(list.findlength()-1);
+		int m = list.findlength()% 2;
+		int a;
+		if (m==0) {
+			a = list.findlength()/2;
+		}
+		else 
+		{
+			a = (list.findlength()-1)/2;
+		}
+		list.findmiddle(a);
 		
 	}
 	
@@ -47,5 +61,18 @@ public class List2 {
 			position ++;
 		}
 	}
+	
+	public void findmiddle(int p) {		
+		int position = 0;
+		Node n = head;
+		while (n!=null) {
+			if (position == p) {
+				System.out.println(n.data);
+			}
+			n=n.next;
+			position ++;
+		}
+	}
+	
 	
 }
