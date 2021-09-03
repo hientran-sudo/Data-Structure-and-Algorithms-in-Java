ee617f2c1242805dc77413903233d1dbcdd655b3
hientran-sudo <hien.tran@student.fairfield.edu>
2021-09-03 10:49:09 -0400

    search element

--- Stack/src/Main.java
+++ Stack/src/Main.java
@@ -10,6 +10,9 @@ public class Main {
 		stack.push(3);
 		System.out.println(stack);
 		System.out.println("top element " + stack.peek());
+		
+		searching(stack,1);
+		
 		try {
 			while (stack.peek()!=null) {
 				stack.pop();
@@ -18,5 +21,15 @@ public class Main {
 		catch (EmptyStackException e) {
 			System.out.println("Stack is empty now");
 		}			
+	}
+	
+	public static void searching (Stack <Integer> stack, int a) {
+		Integer pos = (Integer) stack.search(a);
+		if (pos == -1) {
+			System.out.println("Item not found");
+		}
+		else {
+			System.out.println("element at position " + stack.search(a));
+		}
 	}
 }
