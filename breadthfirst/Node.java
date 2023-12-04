a1473d6d44700c870de1b08127706f217f1b8436
Hien Tran <79175744+hientran-sudo@users.noreply.github.com>
2023-12-04 06:58:47 -0500

    Create Node.java

---
+++ breadthfirst/Node.java
@@ -0,0 +1,36 @@
+
+public class Node {
+	int value;
+	Node left;
+	Node right;
+	
+	Node (int value){
+		this.value = value;
+		left = null;
+		right = null;
+	}
+
+	public Node getLeftChild() {
+		// TODO Auto-generated method stub
+		return left;
+	}
+	
+	public Node getRightChild() {
+		// TODO Auto-generated method stub
+		return right;
+	}
+
+	public void setLeftChild(Node b) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void setRightChild(Node c) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+	
+
+}
